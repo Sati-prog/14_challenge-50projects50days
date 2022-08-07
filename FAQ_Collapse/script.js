223a25bb4e4f864f524084dbdabeba357f9e4050
Sati <rock_sal@mail.ru>
2022-08-07 22:19:01 +0600

    FAQ Collapse

---
+++ FAQ_Collapse/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const toggles = document.querySelectorAll('.faq-toggle');
+
+toggles.forEach(toggle => {
+
+    toggle.addEventListener('click', () => {
+
+        toggle.parentNode.classList.toggle('active');
+    });
+});
